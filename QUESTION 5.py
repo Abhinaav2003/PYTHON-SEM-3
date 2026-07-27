c722968ccb9f68dd1d9af7c7623805822d7e4429
Abhinaav2003 <144618349+Abhinaav2003@users.noreply.github.com>
2026-07-27 23:04:55 +0530

    A

---
+++ QUESTION 5.py
@@ -0,0 +1,8 @@
+# WAP TO PRINT MULTIPLICATION TABLE OF A GIVEN NUMBER ?
+
+num = int(input("Enter a number: "))
+
+print("Multiplication table of", num)
+
+for i in range(1, 11):
+    print(num, "x", i, "=", num*i)
