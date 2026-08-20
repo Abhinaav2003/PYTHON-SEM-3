a98eadb22625a7ec14c118825623c1498fa0478b
Abhinaav2003 <144618349+Abhinaav2003@users.noreply.github.com>
2026-08-20 15:14:50 +0530

    A

--- QUESTION 13.py
+++ QUESTION 13.py
@@ -1,1 +1,16 @@
-  
+# WRITE A PROGRAM TO CHECK WEATHER GIVEN NUMBER IS A PALINDROME OR NOT ?
+
+num = int(input("Enter a number: "))
+
+original = num
+reverse = 0
+
+while num > 0:
+    digit = num % 10
+    reverse = reverse * 10 + digit
+    num = num // 10
+
+if original == reverse:
+    print("Palindrome")
+else:
+    print("Not Palindrome")
